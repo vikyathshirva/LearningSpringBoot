7ba395b12f2267bbf692f71ad42e42b1a9376e33
Vikyath Shirva <vicky.fames@gmail.com>
2021-08-21 21:19:06 +0530

    learning files

---
+++ booting-web/src/main/java/com/vikyath/learn/example/bootingweb/BootingWebApplication.java
@@ -0,0 +1,13 @@
+package com.vikyath.learn.example.bootingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootingWebApplication.class, args);
+	}
+
+}
